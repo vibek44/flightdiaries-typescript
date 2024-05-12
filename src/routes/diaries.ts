c66794ac4cacc33682b15eaa6d3488f02a3e9c03
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-12 18:56:10 +0300

    adding functionality to router.post function to create and validate NewDiaryEntry value type to add new diary and using trycatch block to handle error

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,13 +1,34 @@
 import  express  from "express";
 const router=express.Router();
 import diaryService from "../services/diaryService";
+import getNewDiaryEntry from "../utils";
 
-router.get('/',(_req,res)=>{
+router.get('/', (_req,res)=>{
   res.send(diaryService.getNonSensitiveDiaryEntry());
 });
 
-router.post('/',(_req,res)=>{
-  res.send('saving');
+router.get('/:id', (req,res)=>{
+  const diary=diaryService.findById(Number(req.params.id));
+  if(diary){
+    res.send(diary);
+  }
+  else{
+    res.sendStatus(404);
+  }
+});
+
+router.post('/', (req,res)=>{
+  try {
+    const newDiaryEntry= getNewDiaryEntry(req.body);
+    const addedEntry=diaryService.addDiaries(newDiaryEntry);
+    return res.json(addedEntry);
+  } catch (error:unknown) {
+    let errorMessage='something went wrong ::';
+    if(error instanceof Error){
+      errorMessage=`something went wrong :: ${error.message}`;
+    }
+    return res.sendStatus(400).send(errorMessage);
+  }
 });
 
 export default router;
