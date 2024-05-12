c130b26e4166178f980d52f209fc0a6678b75a61
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-12 21:03:18 +0300

    using enum type for visibility and weather

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,16 @@
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
-export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+export enum Weather { 
+  Sunny='sunny' ,
+  Rainy='rainy' ,
+  Cloudy='cloudy' ,
+  Windy= 'windy' ,
+  Stormy= 'stormy'
+}
+export enum Visibility {
+  Great='great' ,
+  Good='good' ,
+  Ok= 'ok' ,
+  Poor= 'poor'
+}
 
 export interface DiaryEntry {
   id: number;
